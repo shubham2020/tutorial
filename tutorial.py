8ca83d7fc17abc9d9573ab984ed39a8a12072149
Shubham <pshubham.me@gmail.com>
2019-07-27 13:31:51 +0530

    Added a print_song function

--- tutorial.py
+++ tutorial.py
@@ -3,5 +3,9 @@
 def hello():
     print("Hello world")
 
+def print_song(input_is):
+    print("What is a input {}".format(input_is))
+
 if __name__=='__main__':
     hello()
+    print("Shubham Pathak")
